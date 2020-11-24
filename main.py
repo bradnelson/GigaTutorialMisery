a2636479edc272ce2fea2d9ef24c249ee5c44605
Bradley Nelson <b.jamin.nelson@gmail.com>
2020-11-24 00:20:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import sys, pygame
+
+pygame.init()
+
+size = width, height = 320, 240
+speed = [1, 1]
+black = 0, 0, 0
+white = 255, 255, 255
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()
+
+crashed = False
+
+events = ["event1", "event2", "event3"]
+
+while not crashed:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+  
+  ballrect = ballrect.move(speed)
+  if ballrect.left < 0 or ballrect.right > width:
+    speed[0] = -speed[0]
+  if ballrect.top < 0 or ballrect.bottom > height:
+    speed[1] = -speed[1]
+  
+  screen.fill(black)
+  screen.blit(ball, ballrect)
+  pygame.display.flip()
